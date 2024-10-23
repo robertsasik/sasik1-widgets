d727d33425aa93190717a00b7b03945154f139eb
robertsasik <119259754+robertsasik@users.noreply.github.com>
2024-10-23 20:48:12 +0000

    ok

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,9 @@ st.title("🎈 My new app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+
+user = st.secrets["p_user"]
+db_password = st.secrets["p_password"]
+
+
+st.write(f"DB Username: {user}")
