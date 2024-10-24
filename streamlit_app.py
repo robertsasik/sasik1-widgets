48f0b9d3f2256b616bc70ebcd99a63f7481f67bc
robertsasik <119259754+robertsasik@users.noreply.github.com>
2024-10-24 09:24:58 +0000

    ok

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,10 @@ import sqlalchemy
 from sqlalchemy import create_engine
 import geopandas as gpd
 import matplotlib
+import folium
+from streamlit_folium import st_folium
+
+
 
 st.title("🎈 My new app")
 st.write(
@@ -11,6 +15,7 @@ st.write(
 
 user = st.secrets["p_user"]
 db_password = st.secrets["p_password"]
+
 
 
 st.write(f"DB Username: {user}")
@@ -22,10 +27,24 @@ user = st.secrets["p_user"]
 password = st.secrets["p_password"]
 st.write(f"DB Username: {user}")
 
+@st.cache_resource
+def get_db_connection():
+    db_connection_url = f"postgresql://{user}:{password}@{host}:{port}/{database}"
+    engine = create_engine(db_connection_url) 
+    return engine
 
-db_connection_url = f"postgresql://student:{password}@158.193.56.170/postgis_33_sample"
-con = create_engine(db_connection_url) 
+con = get_db_connection()
 sql = "SELECT * FROM hranice_okresy_1"
+#sql = st.text_input("Zadaj SQL skript: ")
 
 gdf = gpd.read_postgis(sql, con, geom_col='geom', crs = 5514)
-gdf.plot()
+gdf
+
+#gdf = gdf.to_crs(epsg=4326)
+
+m = folium.Map(location=[48.14816, 17.10674], zoom_start=8) 
+
+folium.GeoJson(gdf).add_to(m)
+
+st_folium(m, width=800, height=600)
+
